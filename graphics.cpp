8177c0cbee1a16a7c3893f433ce3003f8a0d6d3c
Bui Minh Hoa <24022791@vnu.edu.vn>
2025-04-12 22:56:55 +0700

    Update graphics.cpp

--- graphics.cpp
+++ graphics.cpp
@@ -1,5 +1,3 @@
-
-#include "common_func.h"
 #include "graphics.h"
 
 const char* KONGRUN_SPRITE_FILE = "D:/projectBTL/bananakong/image/CHAR/kong_run.png";
@@ -24,30 +22,23 @@ Graphics::Graphics(SDL_Renderer* renderer) : renderer(renderer) {}
 
 Graphics::~Graphics() {}
 
-SDL_Texture* Graphics::loadTexture(const char *filename) {
-    SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO,
-                   "Loading %s", filename);
-
-    SDL_Texture *texture = IMG_LoadTexture(renderer, filename);
+SDL_Texture* Graphics::loadTexture(const char* filename) {
+    SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO, "Loading %s", filename);
+    SDL_Texture* texture = IMG_LoadTexture(renderer, filename);
     if (texture == NULL) {
-        SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR,
-                       "Load texture %s", IMG_GetError());
+        SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "Load texture %s", IMG_GetError());
     }
-
     return texture;
 }
 
-void Graphics::renderTexture(SDL_Texture *texture, int x, int y) {
+void Graphics::renderTexture(SDL_Texture* texture, int x, int y) {
     SDL_Rect dest;
     dest.x = x;
     dest.y = y;
-
-    // Truy vấn kích thước thực của texture
     int texWidth, texHeight;
     SDL_QueryTexture(texture, NULL, NULL, &texWidth, &texHeight);
     dest.w = texWidth;
     dest.h = texHeight;
-
     SDL_RenderCopy(renderer, texture, NULL, &dest);
 }
 
@@ -66,55 +57,10 @@ void Graphics::presentScene() {
     SDL_RenderPresent(renderer);
 }
 
-// Thêm hàm render cho ScrollingBackground
 void Graphics::render(const ScrollingBackground& bgr) {
     int x = bgr.scrollingOffset;
-    // Vẽ nền liên tục cho đến khi phủ kín màn hình
     while (x > -bgr.width) {
         renderTexture(bgr.texture, x, 0);
         x -= bgr.width;
-    }
-}
-
-//sinh chướng ngại vật
-/*void spawnObstacle(vector<Obstacle>& obstacles, SDL_Texture* texture){
-    Obstacle obs;
-    obs.texture = texture;
-    obs.rect.x = SCREEN_WIDTH;
-    obs.rect.y = 745; // căn theo nền
-    obs.rect.w = 230;
-    obs.rect.h = 156;
-    obstacles.push_back(obs);
-}*/
-
-void spawnObstacleSingle(Obstacle& obs, const map<ObstacleType, SDL_Texture*>& textureMap, int offsetX) {
-    // Chọn loại chướng ngại vật ngẫu nhiên
-    ObstacleType type = static_cast<ObstacleType>(rand() % 3);
-    obs.type = type;
-    obs.texture = textureMap.at(type);
-    obs.rect.x = SCREEN_WIDTH + offsetX;
-    obs.rect.y = 745; // căn theo nền
-    switch (type) {
-        case ObstacleType::ROCK:
-            obs.rect.w = 213;
-            obs.rect.h = 264;
-            break;
-        case ObstacleType::SPIKE:
-            obs.rect.w = 100;
-            obs.rect.h = 120;
-            break;
-        case ObstacleType::LOG:
-            obs.rect.w = 449;
-            obs.rect.h = 177;
-            break;
-    }
-}
-
-void spawnObstacle(std::vector<Obstacle>& obstacles, const map<ObstacleType, SDL_Texture*>& textureMap) {
-    int numToSpawn = rand() % 2 + 1; // 1 hoặc 2 vật cản
-    for (int i = 0; i < numToSpawn; ++i) {
-        Obstacle obs;
-        spawnObstacleSingle(obs, textureMap, i * 150); // Mỗi obstacle cách nhau 150px
-        obstacles.push_back(obs);
     }
 }
