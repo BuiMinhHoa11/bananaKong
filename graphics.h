4cae01bbd69a94c558ab8094c5359507d45dc17c
Bui Minh Hoa <24022791@vnu.edu.vn>
2025-04-12 22:57:05 +0700

    Update graphics.h

--- graphics.h
+++ graphics.h
@@ -1,13 +1,14 @@
-#ifndef GRAPHICS__H_
-#define GRAPHICS__H_
+#ifndef GRAPHICS_H_
+#define GRAPHICS_H_
 
 #include "common_func.h"
 
+// Hằng số cho sprite của Kong
 extern const char* KONGRUN_SPRITE_FILE;
 extern const int KONGRUN_CLIPS[][4];
 extern const int KONGRUN_FRAMES;
 
-//scrolling background
+// Scrolling background
 struct ScrollingBackground {
     SDL_Texture* texture;
     int scrollingOffset = 0;
@@ -20,17 +21,21 @@ struct ScrollingBackground {
 
     void scroll(int distance) {
         scrollingOffset -= distance;
-        if( scrollingOffset < 0 ) { scrollingOffset = width; }
+        if (scrollingOffset < 0) {
+            scrollingOffset = width;
+        }
     }
 };
 
-//char animation
+// Sprite cho animation
 struct Sprite {
     SDL_Texture* texture;
     std::vector<SDL_Rect> clips;
     int currentFrame = 0;
+    int frameDelay = 0;
+    int frameDelayMax = 5;
 
-    void init(SDL_Texture* _texture, int frames, const int _clips [][4]) {
+    void init(SDL_Texture* _texture, int frames, const int _clips[][4]) {
         texture = _texture;
         SDL_Rect clip;
         for (int i = 0; i < frames; i++) {
@@ -41,8 +46,17 @@ struct Sprite {
             clips.push_back(clip);
         }
     }
+
     void tick() {
-        currentFrame = (currentFrame + 1) % clips.size();
+        frameDelay++;
+        if (frameDelay >= frameDelayMax) {
+            currentFrame = (currentFrame + 1) % clips.size();
+            frameDelay = 0;
+        }
+    }
+
+    void setAnimationSpeed(int speed) {
+        frameDelayMax = speed;
     }
 
     const SDL_Rect* getCurrentClip() const {
@@ -55,58 +69,15 @@ public:
     Graphics(SDL_Renderer* renderer);
     ~Graphics();
 
-    SDL_Texture* loadTexture(const char *filename);
-    void renderTexture(SDL_Texture *texture, int x, int y);
+    SDL_Texture* loadTexture(const char* filename);
+    void renderTexture(SDL_Texture* texture, int x, int y);
     void render(int x, int y, const Sprite& sprite);
     void prepareScene();
     void presentScene();
-    // Thêm hàm render cho ScrollingBackground
     void render(const ScrollingBackground& bgr);
+
 private:
     SDL_Renderer* renderer;
 };
 
-/*struct Obstacle {
-    SDL_Rect rect;
-    SDL_Texture* texture;
-
-    void move(int speed) {
-        rect.x -= speed;
-    }
-
-    bool isOffScreen() const {
-        return rect.x + rect.w < 0;
-    }
-};*/
-
-//sinh chướng ngại vật
-//void spawnObstacle(vector<Obstacle>& obstacles, SDL_Texture* texture);
-
-enum class ObstacleType {
-    ROCK,
-    SPIKE,
-    LOG
-};
-
-struct Obstacle {
-    SDL_Rect rect;
-    SDL_Texture* texture;
-    ObstacleType type;
-
-    void move(int speed) {
-        rect.x -= speed;
-    }
-
-    bool isOffScreen() const {
-        return rect.x + rect.w < 0;
-    }
-};
-
-
-
-// Khai báo
-void spawnObstacle(vector<Obstacle>& obstacles, const map<ObstacleType, SDL_Texture*>& textureMap);
-void spawnObstacleSingle(Obstacle& obs, const map<ObstacleType, SDL_Texture*>& textureMap, int offsetX = 0);
-
-
-#endif // GRAPHICS__H_
+#endif // GRAPHICS_H_
