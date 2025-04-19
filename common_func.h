5eee6a90c142f97221a0f7eec8388d64a0a4c31c
Bui Minh Hoa <24022791@vnu.edu.vn>
2025-04-19 21:43:43 +0700

    Update common_func.h

--- common_func.h
+++ common_func.h
@@ -17,15 +17,12 @@ const int SCREEN_HEIGHT = 963;
 const int SCREEN_BPP = 72;
 const string WINDOW_TITLE = "BANANA KONG";
 
-const int COLOR_KEY_R = 255;
-const int COLOR_KEY_G = 255;
-const int COLOR_KEY_B = 255;
-
-const int RENDER_DRAW_COLOR = 0xff;
-
 void initSDL(SDL_Window* &window, SDL_Renderer* &renderer);
 void logSDLError(ostream& os, const string &msg, bool fatal = false);
 void quitSDL(SDL_Window* window, SDL_Renderer* renderer);
 void waitUntilKeyPressed();
+
+void initTTF();
+SDL_Texture* createTextTexture(SDL_Renderer* renderer, const char* text, TTF_Font* font, SDL_Color color, int& width, int& height);
 
 #endif // COMMON_FUNC__H_
