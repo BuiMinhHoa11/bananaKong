9f9e0e9461cf81e03c1ce4db4ca30d0e643c33c8
Bui Minh Hoa <24022791@vnu.edu.vn>
2025-05-04 16:04:47 +0700

    Update common_func.h

--- common_func.h
+++ common_func.h
@@ -19,7 +19,7 @@ const string WINDOW_TITLE = "BANANA KONG";
 
 const int GROUND_LEVEL = 885;
 const int KONG_HEIGHT = 150;
-const int KONG_DRAW_Y_START = GROUND_LEVEL - KONG_HEIGHT; // 888 - 150 = 738
+const int KONG_DRAW_Y_START = GROUND_LEVEL - KONG_HEIGHT + 10; // 888 - 150 = 738
 
 void initSDL(SDL_Window* &window, SDL_Renderer* &renderer);
 void logSDLError(ostream& os, const string &msg, bool fatal = false);
