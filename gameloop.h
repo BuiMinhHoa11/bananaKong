e0d030bb51dc11df07db0677f6217a54b0770163
Bui Minh Hoa <24022791@vnu.edu.vn>
2025-05-05 08:43:23 +0700

    Create gameloop.h

---
+++ gameloop.h
@@ -0,0 +1,49 @@
+#ifndef GAMELOOP_H
+#define GAMELOOP_H
+
+#include "common_func.h"
+#include "graphics.h"
+#include "obstacle.h"
+#include "player.h"
+#include "platform.h"
+#include "banana.h"
+#include "menu.h"
+
+class GameLoop {
+public:
+    GameLoop(Graphics& graphics, Player& player, ObstacleManager& obstacleManager,
+             PlatformManager& platformManager, BananaManager& bananaManager,
+             ScrollingBackground& backgroundSky, ScrollingBackground& background,
+             ScrollingBackground& leafTop);
+    void handleEvents(SDL_Event& e, GameState& gameState);
+    void update(GameState& gameState, float deltaTime);
+    void render(Graphics& graphics, GameState gameState, TTF_Font* font);
+    void reset();
+
+    int getDistance() const { return distance; }
+    int getBestDistance() const { return bestDistance; }
+    int getCurrentBananas() const { return currentBananas; }
+    int getTotalBananas() const { return totalBananas; }
+    int getBestBananas() const { return bestBananas; }
+
+private:
+    Player& kong;
+    ObstacleManager& obstacleManager;
+    PlatformManager& platformManager;
+    BananaManager& bananaManager;
+    ScrollingBackground& backgroundSky;
+    ScrollingBackground& background;
+    ScrollingBackground& leafTop;
+
+    bool isSpaceHeld;
+    Uint32 lastFrameTime;
+    float animationUpdateTimer;
+    float scoreTimer;
+    int distance;
+    int bestDistance;
+    int currentBananas;
+    int totalBananas;
+    int bestBananas;
+};
+
+#endif // GAMELOOP_H
