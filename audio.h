99c0608ab6c1de33db1f75fae2519fbf7fe782fe
Bui Minh Hoa <24022791@vnu.edu.vn>
2025-05-06 23:25:59 +0700

    Create audio.h

---
+++ audio.h
@@ -0,0 +1,45 @@
+#ifndef AUDIO_H
+#define AUDIO_H
+
+#include <SDL_mixer.h>
+#include <map>
+#include <string>
+
+enum class SoundType {
+    DIE,
+    COUNT_DOWN,
+    EAT,
+    JUMP,
+    CLIMB_DOWN,
+    IDLE,
+    SLIDE,
+    CLICK
+};
+
+enum class MusicType {
+    HOMEPLAY,
+    LOOP
+};
+
+class AudioManager {
+public:
+    AudioManager();
+    ~AudioManager();
+
+    bool init();
+    void loadSounds();
+    void playSound(SoundType type);
+    void playMusic(MusicType type);
+    void stopMusic();
+    void setMusicEnabled(bool enabled);
+    void setSoundEnabled(bool enabled);
+
+private:
+    std::map<SoundType, Mix_Chunk*> soundEffects;
+    std::map<MusicType, Mix_Music*> backgroundMusic;
+    bool musicEnabled;
+    bool soundEnabled;
+    MusicType currentMusic;
+};
+
+#endif // AUDIO_H
